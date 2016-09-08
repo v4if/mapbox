03000140891e668e6006e405f1d5831a77340416
atkjest <atkjest@gmail.com>
2016-09-08 18:36:11 +0800

    update framework

---
+++ README.txt
@@ -0,0 +1,6 @@
+## 我和她的Web版足迹地图
+博客文章地址： [v4if](http://v4if.me/2016/ForBunny/)
+![forbunny](http://7xot8c.com1.z0.glb.clouddn.com/2016-09-08-182725.png)
+上线地址： [v4if.me/mapbox](http://v4if.me/mapbox)
+应用部署效果图：
+![forbunny](http://7xot8c.com1.z0.glb.clouddn.com/2016-09-08-172406.png)
---
+++ js/mapbox_feature_geojson.js
@@ -0,0 +1,173 @@
+var geojson = [
+{
+  "type": "FeatureCollection",
+  "features": [
+	{
+	  "type": "Feature",
+	  "properties": {
+		  "id": "marker-isk0a6854",
+		  "title": "通化 — 东北姑娘",
+		  "details": "那一年我们都不知道对方的存在，若干年后对彼此许下一生的承诺",
+		  "description": "<img src='http://od4lcpo9k.bkt.clouddn.com/b_home.jpg' alt='forbunny'>那一年我们都不知道对方的存在，若干年后对彼此许下一生的承诺",
+		  "marker-size": "medium",
+		  "marker-color": "#ffffff",
+		  "marker-symbol": "warehouse"
+	  },
+	  "geometry": {
+		  "coordinates": [
+		  125.919799,
+		  41.73033
+		  ],
+		  "type": "Point"
+	  },
+	  "id": "2f13a0089cef4d606e67ffa28df48c2a"
+  },
+  {
+	  "type": "Feature",
+	  "properties": {
+		  "id": "marker-isk0lx6e6",
+		  "title": "济宁 — 山东小伙",
+		  "details": "那一年我们都不知道对方的存在，若干年后对彼此许下一生的承诺",
+		  "description": "<img src='http://od4lcpo9k.bkt.clouddn.com/IMG_20150806_124627.jpg' alt='forbunny'>那一年我们都不知道对方的存在，若干年后对彼此许下一生的承诺",
+		  "marker-size": "medium",
+		  "marker-color": "#ffffff",
+		  "marker-symbol": "warehouse"
+  	},
+	  "geometry": {
+		  "coordinates": [
+		  116.582794,
+		  35.414795
+		  ],
+		  "type": "Point"
+	  },
+	  "id": "3607967b854de51956dbee94f5df64b7"
+  },
+  {
+	  "type": "Feature",
+	  "properties": {
+		  "id": "marker-isk01a7p1",
+		  "title": "青岛 — 相遇相爱篇",
+		  "details": "风吹起你的头发，一张棕色的小网撒满我的面颊，我一生也不想挣脱",
+		  "description": "<img src='http://od4lcpo9k.bkt.clouddn.com/74962bf9a53ceb34.jpg' alt='forbunny'>风吹起你的头发，一张棕色的小网撒满我的面颊，我一生也不想挣脱",
+		  "marker-size": "medium",
+		  "marker-color": "#ffffff",
+		  "marker-symbol": "college"
+	  },
+	  "geometry": {
+		  "coordinates": [
+		  120.344238,
+		  36.097938
+		  ],
+		  "type": "Point"
+	  },
+	  "id": "5595e5482935805cc25660574669bf86"
+  },
+  {
+	  "type": "Feature",
+	  "properties": {
+		  "id": "marker-isjzvwlr0",
+		  "title": "曲阜 — 游山玩水篇",
+		  "details": "世界那么大，我们一起去看看，周游世界，然后记录下我们的足迹地图",
+		  "description": "<img src='http://od4lcpo9k.bkt.clouddn.com/IMG20151003094748.jpg' alt='forbunny'>世界那么大，我们一起去看看，周游世界，然后记录下我们的足迹地图",
+		  "marker-size": "medium",
+		  "marker-color": "#ffffff",
+		  "marker-symbol": "camera"
+	  },
+	  "geometry": {
+		  "coordinates": [
+		  116.9833,
+		  35.6
+		  ],
+		  "type": "Point"
+	  },
+	  "id": "a13e67e2c59bc040d0fdc268474fb22b"
+  },
+  {
+	  "type": "Feature",
+	  "properties": {
+		  "id": "marker-isk03qlp2",
+		  "title": "北京 — 你在这",
+		  "details": "1035公里的距离，短暂的相聚后留下的只是回忆，但是彼此的心连在一起",
+		  "description": "<img src='http://od4lcpo9k.bkt.clouddn.com/IMG20150920065237.jpg' alt='forbunny'>1035公里的距离，短暂的相聚后留下的只是回忆，但是彼此的心连在一起",
+		  "marker-size": "medium",
+		  "marker-color": "#ffffff",
+		  "marker-symbol": "rocket"
+	  },
+	  "geometry": {
+		  "coordinates": [
+		  116.38916,
+		  39.918162
+		  ],
+		  "type": "Point"
+	  },
+	  "id": "b9c467e40ee87ba1c8dc0af537e0feec"
+  },
+  {
+	  "type": "Feature",
+	  "properties": {
+		  "id": "marker-isk06d3h3",
+		  "title": "西安 — 我在这",
+		  "details": "1035公里的距离，短暂的相聚后留下的只是回忆，但是彼此的心连在一起",
+		  "description": "<img src='http://od4lcpo9k.bkt.clouddn.com/IMG20160828044527.jpg' alt='forbunny'>1035公里的距离，短暂的相聚后留下的只是回忆，但是彼此的心连在一起",
+		  "marker-size": "medium",
+		  "marker-color": "#ffffff",
+		  "marker-symbol": "rocket"
+	  },
+	  "geometry": {
+		  "coordinates": [
+		  108.929443,
+		  34.375178
+		  ],
+		  "type": "Point"
+	  },
+	  "id": "4550dbd23b2eac654860ec82dfb2eb79"
+  },
+  {
+	  "type": "Feature",
+	  "properties": {
+		  "id": "marker-isk0e8ce5",
+		  "title": "西藏 — 杜鹃花",
+		  "details": "你说你想去看漫山遍野的杜鹃花，色季拉山位于西藏自治区林芝地区林芝县以东，最出名的便是那满山遍野的杜鹃花",
+		  "description": "<img src='http://od4lcpo9k.bkt.clouddn.com/1456471994_MpPgHY.jpg' alt='forbunny'>你说你想去看漫山遍野的杜鹃花，色季拉山位于西藏自治区林芝地区林芝县以东，最出名的便是那满山遍野的杜鹃花",
+		  "marker-size": "medium",
+		  "marker-color": "#ffffff",
+		  "marker-symbol": "golf"
+	  },
+	  "geometry": {
+		  "coordinates": [
+		  91.1,
+		  29.6
+		  ],
+		  "type": "Point"
+	  },
+	  "id": "a859c2dc411102eb5907cbc4bd38e9af"
+  },
+  {
+	  "type": "Feature",
+	  "properties": {
+		  "title": "你我之间的连线",
+		  "details": "你我之间的连线",
+		  "description": "1035公里的距离，短暂的相聚后留下的只是回忆，但是彼此的心连在一起",
+		  "stroke": "#1087bf",
+		  "stroke-width": 4,
+		  "stroke-opacity": 1
+	  },
+	  "geometry": {
+		  "coordinates": [
+		  [
+		  116.38916,
+		  39.918162
+		  ],
+		  [
+		  108.929443,
+		  34.375178
+		  ]
+		  ],
+		  "type": "LineString"
+	  },
+	  "id": "63150d05569345f5864e98bf255f7c6c"
+  }
+  ],
+  "id": "v4if.199afpm4"
+}
+];
